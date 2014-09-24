046a2a5424b26f682151d8a1c05b6576cf6570a3
Rolf Erik Lekang <me@rolflekang.com>
2014-09-24 16:32:18 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+setup(
+    name="django-auth-abakus",
+    version='0.1.0',
+    url='http://github.com/webkom/django-auth-',
+    author='Webkom, Abakus Linjeforening',
+    author_email='webkom@abakus.no',
+    description='A django auth module that can be used to to authenticate '
+                'users against the API of abakus.no.',
+    packages=find_packages(exclude='tests'),
+    tests_require=[
+        'django>=1.4',
+    ],
+    license='MIT',
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ]
+)
