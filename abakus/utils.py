18e2293da5fa9dc352519bb5065fd47e1ac4950a
Rolf Erik Lekang <me@rolflekang.com>
2015-03-13 14:54:41 +0100

    Fix pep8

--- abakus/utils.py
+++ abakus/utils.py
@@ -12,5 +12,7 @@ if django.VERSION >= (1, 5):
 else:
     from django.contrib.auth.models import User
     AUTH_USER_MODEL = 'auth.User'
-    get_user_model = lambda: User
-    get_username_field = lambda: 'username'
+
+    def get_user_model(): return User
+
+    def get_username_field(): return 'username'
