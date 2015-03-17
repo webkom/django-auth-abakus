62f680abf601832962550ea73eda34530ef4b267
Edvard Bakken <edvard.vb@live.no>
2015-03-17 21:02:13 +0100

    remove unused function or something

--- abakus/utils.py
+++ abakus/utils.py
@@ -8,13 +8,9 @@ if django.VERSION >= (1, 5):
     from django.contrib.auth import get_user_model
     AUTH_USER_MODEL = settings.AUTH_USER_MODEL
     get_user_model = allow_lazy(get_user_model, AUTH_USER_MODEL)
-    get_username_field = allow_lazy(lambda: get_user_model().USERNAME_FIELD, str)
 else:
     from django.contrib.auth.models import User
     AUTH_USER_MODEL = 'auth.User'
 
     def get_user_model():
         return User
-
-    def get_username_field():
-        return 'username'
