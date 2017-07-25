ce4517da60c7c161622823ebd2a2f66c9d87442c
Doppins <contact@doppins.com>
2017-07-25 15:27:21 +0000

    Upgrade dependency requests to ==2.18.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('abakus/__init__.py', 'r') as fd:
         re.MULTILINE
     ).group(1)
 
-install_requires = ['requests==2.18.1']
+install_requires = ['requests==2.18.2']
 tests_require = [
     'django>=1.4',
     'responses'
